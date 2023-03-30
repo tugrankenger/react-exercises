22ec0ca855bc53631ba4d9d2dc74738af0bb9f8b
tugrankenger <turankenger@hotmail.com.tr>
2023-03-30 22:34:41 +0300

    apply usememo filteredUsers function

--- react-usememo/src/App.js
+++ react-usememo/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useMemo, useState } from 'react';
 import './App.css';
 
+const users = [
+  { id: 'a', name: 'tugran' },
+  { id: 'b', name: 'juliet' },
+  { id: 'c', name: 'desmond' },
+  { id: 'd', name: 'kate' },
+  { id: 'e', name: 'james' },
+];
 function App() {
+  const [text, setText] = useState('');
+  const [search, setSearch] = useState('');
+
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
+  const handleSearch = () => {
+    setSearch(text);
+  };
+
+  // The filteredUsers will work only when search is triggered
+  const filteredUsers = useMemo(() => {
+    return users.filter((item) => {
+      console.log('filter function is running');
+      return item.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }, [search]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        type='text'
+        value={text}
+        onChange={handleText}
+        placeholder='Search user...'
+      />
+      <button type='button' onClick={handleSearch}>
+        Search
+      </button>
+      <List list={filteredUsers} />
     </div>
   );
 }
+
+const List = ({ list }) => {
+  return (
+    <ul>
+      {list.map((item) => (
+        <ListItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const ListItem = ({ item }) => {
+  return <li>{item.name}</li>;
+};
 
 export default App;
