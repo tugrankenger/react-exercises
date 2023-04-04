3314be8830071538495fa677c143b27f07fce130
tugrankenger <turankenger@hotmail.com.tr>
2023-04-04 09:46:01 +0300

    create html template

--- react-redux-todo/src/App.js
+++ react-redux-todo/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const INITIAL_STATE = [
+  {
+    id: 1,
+    title: 'Shopping',
+    status: false,
+  },
+  {
+    id: 2,
+    title: 'Play CS:GO',
+    status: true,
+  },
+  {
+    id: 3,
+    title: 'Read book',
+    status: true,
+  },
+];
+
 function App() {
+  const [list, setList] = useState(INITIAL_STATE);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Todo List</h1>
+      <div className='add-form'>
+        <input type='text' placeholder='Add item' />
+        <button className='add-btn'>Add</button>
+      </div>
+      <div className='list'>
+        {list.map((item) => (
+          <div className={item.status ? 'done' : ''} key={item.id}>
+            {item.title}
+          </div>
+        ))}
+      </div>
+      <button className='remove-btn'>Remove that completed</button>
     </div>
   );
 }
