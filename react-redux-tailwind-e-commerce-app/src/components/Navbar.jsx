cd0888a762d8fad0e82c0bf94681cc74a7650956
tugrankenger <turankenger@hotmail.com.tr>
2023-04-08 14:12:55 +0300

    create navbar and dark mode

--- react-redux-tailwind-e-commerce-app/src/components/Navbar.jsx
+++ react-redux-tailwind-e-commerce-app/src/components/Navbar.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { BsLightbulb, BsFillMoonStarsFill } from 'react-icons/bs';
+import { SlBasket } from 'react-icons/sl';
+import { HiShoppingBag } from 'react-icons/hi';
 
 function Navbar() {
+  const [color, setColor] = useState(false);
+  useEffect(() => {
+    const root = document.getElementById('root');
+    if (color) {
+      root.style.background = '#000000';
+      root.style.color = '#efefef';
+    } else {
+      root.style.background = '#FFFFFF';
+      root.style.color = '#000000';
+    }
+  }, [color]);
   return (
-    <div>Navbar</div>
-  )
+    <div className='flex items-center justify-between px-3 h-20'>
+      <div className='bg-blue-400 rounded-full p-2'>
+        <HiShoppingBag size={40} />
+      </div>
+      <div className='flex space-x-4 items-center'>
+        <input
+          type='text'
+          placeholder='search...'
+          className='outline-none border p-3 rounded-lg'
+        />
+        <div onClick={() => setColor(!color)}>
+          {color ? (
+            <BsFillMoonStarsFill size={25} className='cursor-pointer' />
+          ) : (
+            <BsLightbulb size={25} className='cursor-pointer' />
+          )}
+        </div>
+        <div className='relative'>
+          <SlBasket className='cursor-pointer' size={25} />
+          <span className='absolute -top-2 -right-2 px-1 rounded-full bg-red-600 text-white text-sm'>
+            3
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- react-redux-tailwind-e-commerce-app/src/containers/PageContainer.jsx
+++ react-redux-tailwind-e-commerce-app/src/containers/PageContainer.jsx
@@ -1,7 +1,7 @@
 
 const PageContainer = ({children}) =>{
   return (
-    <div className='w-3/4 m-auto bg-gray-100'>{children}</div>
+    <div className='w-3/4 m-auto'>{children}</div>
   )
 }
 
