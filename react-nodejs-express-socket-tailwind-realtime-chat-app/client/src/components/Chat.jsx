c531b0ac99a72ab5aa2ffb879eaea2290f16d1bc
tugrankenger <turankenger@hotmail.com.tr>
2023-05-10 19:10:42 +0300

    send messages between users in the same room

--- react-nodejs-express-socket-tailwind-realtime-chat-app/client/src/components/Chat.jsx
+++ react-nodejs-express-socket-tailwind-realtime-chat-app/client/src/components/Chat.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Chat({ socket, username, room }) {
   const [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
 
-  const sendMessage = async() => {
-    const date = new Date
+  useEffect(() => {
+    socket.on('returnMessage', (data) => {
+      setMessageList((prev) => [...prev, data]);
+    });
+  }, [socket]);
+
+  const sendMessage = async () => {
+    const date = new Date();
     const messageContent = {
       username: username,
       message: message,
       room: room,
-      date: date.getHours() + ':' + date.getMinutes()
-    }
+      date: date.getHours() + ':' + date.getMinutes(),
+    };
     await socket.emit('message', messageContent);
-
+    setMessageList((prev) => [...prev, messageContent]);
+    setMessage('');
   };
+  // console.log(messageList);
   return (
     <div className='flex items-center justify-center h-full'>
       <div className='w-1/3 h-[600px] bg-chat-image relative'>
@@ -21,20 +30,30 @@ function Chat({ socket, username, room }) {
           <div className='rounded-full w-12 h-12 bg-white'></div>
         </div>
         <div className='w-full h-[400px] overflow-y-auto'>
-          <div className='w-1/2 h-12 p-1 bg-indigo-600 text-white m-2 rounded-xl rounded-br-none'>
-            <div>test message</div>
-            <div className='w-full flex justify-end text-xs'>
-              tugrankenger - 09.05.2023
-            </div>
-          </div>
-          <div className='flex justify-end'>
-            <div className='w-1/2 h-12 p-1 bg-green-600 text-white m-2 rounded-xl rounded-br-none'>
-              <div>test message</div>
-              <div className='w-full flex justify-end text-xs'>
-                tugrankenger - 09.05.2023
-              </div>
-            </div>
-          </div>
+          {messageList &&
+            messageList.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`${
+                    username === item.username ? 'flex justify-end' : ''
+                  }`}
+                >
+                  <div
+                    className={`${
+                      username === item.username
+                        ? ' bg-indigo-600'
+                        : 'bg-green-600 '
+                    } w-1/2 p-1 text-white m-2 rounded-xl rounded-br-none `}
+                  >
+                    <div>{item.message}</div>
+                    <div className='w-full flex justify-end text-xs'>
+                      {item.username} - {item.date}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
         <div className='absolute bottom-0 left-0 w-full flex bg-gray-200 opacity-90 p-3 gap-x-2'>
           <input
