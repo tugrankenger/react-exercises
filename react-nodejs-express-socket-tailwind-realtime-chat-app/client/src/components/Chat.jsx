34c5e13a523a139b1b4be0ad1e95a1d1197aa7ea
tugrankenger <turankenger@hotmail.com.tr>
2023-05-11 12:18:36 +0300

    add scrollablefeed, onKeydown

--- react-nodejs-express-socket-tailwind-realtime-chat-app/client/src/components/Chat.jsx
+++ react-nodejs-express-socket-tailwind-realtime-chat-app/client/src/components/Chat.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import ScrollableFeed from 'react-scrollable-feed';
+import { MdSend } from 'react-icons/md';
 
 function Chat({ socket, username, room }) {
   const [message, setMessage] = useState('');
@@ -22,52 +24,61 @@ function Chat({ socket, username, room }) {
     setMessageList((prev) => [...prev, messageContent]);
     setMessage('');
   };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && message !== '') {
+      sendMessage();
+    }
+  };
   // console.log(messageList);
   return (
-    <div className='flex items-center justify-center h-full'>
-      <div className='w-1/3 h-[600px] bg-chat-image relative'>
+    <div className='flex items-center justify-center h-full '>
+      <div className='md:w-1/3 w-full md:h-[600px] h-full  bg-chat-image relative '>
         <div className='w-full h-16 bg-gray-200 opacity-95 flex items-center p-3'>
           <div className='rounded-full w-12 h-12 bg-white'></div>
         </div>
-        <div className='w-full h-[400px] overflow-y-auto'>
-          {messageList &&
-            messageList.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className={`${
-                    username === item.username ? 'flex justify-end' : ''
-                  }`}
-                >
+        <div className='w-full h-[465px] overflow-y-auto '>
+          <ScrollableFeed forceScroll={true}>
+            {messageList &&
+              messageList.map((item, index) => {
+                return (
                   <div
+                    key={index}
                     className={`${
-                      username === item.username
-                        ? ' bg-indigo-600'
-                        : 'bg-green-600 '
-                    } w-1/2 p-1 text-white m-2 rounded-xl rounded-br-none `}
+                      username === item.username ? 'flex justify-end' : ''
+                    }`}
                   >
-                    <div>{item.message}</div>
-                    <div className='w-full flex justify-end text-xs'>
-                      {item.username} - {item.date}
+                    <div
+                      className={`${
+                        username === item.username
+                          ? ' bg-indigo-600'
+                          : 'bg-green-600 '
+                      } w-1/2 p-1 text-white m-2 rounded-xl rounded-br-none `}
+                    >
+                      <div className='pl-1'>{item.message}</div>
+                      <div className='w-full flex justify-end text-xs'>
+                        {item.username} - {item.date}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+          </ScrollableFeed>
         </div>
-        <div className='absolute bottom-0 left-0 w-full flex bg-gray-200 opacity-90 p-3 gap-x-2'>
+        <div className='absolute bottom-0 left-0 w-full flex justify-between bg-gray-200 opacity-90 p-3 gap-x-2'>
           <input
+            onKeyDown={handleKeyDown}
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            className='w-3/4 border-none rounded-full outline-none p-3 h-12'
+            className='w-full border-none rounded-full outline-none p-3 h-12'
             placeholder='Entry message...'
             type='text'
           />
           <button
             onClick={sendMessage}
-            className='w-1/4 bg-indigo-600 rounded-full text-white p-3 h-12 text-xl flex items-center justify-center tracking-wider'
+            className='w-14 h-12 rounded-full  bg-indigo-600 flex items-center justify-center text-white text-xl'
           >
-            Send
+            <MdSend />
           </button>
         </div>
       </div>
