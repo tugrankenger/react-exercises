4717bed557f220fd6c9e3c01f1a8e5e91b0c2810
tugrankenger <turankenger@hotmail.com.tr>
2023-05-15 22:18:45 +0300

    handle form validation

--- react-nodejs-express-socket-tailwind-realtime-chat-app/client/src/components/Room.jsx
+++ react-nodejs-express-socket-tailwind-realtime-chat-app/client/src/components/Room.jsx
@@ -7,9 +7,13 @@ function Room({ username, setUsername, room, setRoom, socket }) {
   const navigate = useNavigate();
   const [selected, setSelected] = useState(null);
   const [showPopover, setShowPopover] = useState(false);
+  const [errName, setErrName] = useState(false);
+  const [errRoom, setErrRoom] = useState(false);
+  const [errImg, setErrImg] = useState(false);
   const { selectedImage, setSelectedImage } = useContext(DataContext);
   const { images } = useContext(DataContext);
   console.log('selectedContextData:', selectedImage);
+
   useEffect(() => {
     console.log('the selected has changed', selected);
   }, [selected]);
@@ -21,45 +25,99 @@ function Room({ username, setUsername, room, setRoom, socket }) {
     setSelectedImage(event.id);
     console.log(selected);
   };
+
+  const handleValidation = () => {
+    let formIsValidName = true;
+    let formIsValidRoom = true;
+    let formIsValidImg = true;
+    if (username.length < 3 || username == '') {
+      setErrName(true);
+      formIsValidName = false
+    } else {
+      setErrName(false);
+      formIsValidName = true
+    }
+    if (room.length == '') {
+      setErrRoom(true);
+      formIsValidRoom = false
+    } else {
+      setErrRoom(false);
+      formIsValidRoom = true
+    }
+    if (selected == null) {
+      setErrImg(true);
+      formIsValidImg = false
+    } else {
+      setErrImg(false);
+      formIsValidImg = true
+    }
+    console.log(formIsValidName, formIsValidRoom , formIsValidImg);
+    console.log(formIsValidName && formIsValidRoom && formIsValidImg);
+    return (formIsValidName && formIsValidRoom && formIsValidImg)
+  };
+
   const sendRoom = () => {
     socket.emit('room', room);
-    navigate('/chat');
+    if(handleValidation()){
+      navigate('/chat');
+    } 
   };
   return (
     <div className='flex items-center justify-center h-full'>
-      <div className='w-[350px] h-[460px] bg-white shadow-xl flex flex-col items-center justify-center gap-y-4 rounded-lg px-5'>
+      <div className='w-[350px] h-[460px] bg-white shadow-xl flex flex-col items-center justify-center gap-y-3 rounded-lg px-5'>
         <h1 className='font-bold text-2xl text-center text-indigo-600 tracking-wider'>
           WELCOME TO CHAT
         </h1>
-        <div className='w-full'>
-          <input
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className='border focus:border-indigo-700 p-3 outline-none w-full rounded-lg'
-            type='text'
-            placeholder='Username'
-          />
+        <div className='w-full flex flex-col'>
+          <div className='w-full '>
+            <input
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className={`${
+                errName ? 'border-red-600' : ''
+              } border focus:border-indigo-700 p-3 outline-none w-full rounded-lg`}
+              type='text'
+              placeholder='Username'
+            />
+          </div>
+          <div className='errSection h-5'>
+            {errName && (
+              <span className='text-xs text-red-600'>
+                *name is must be more than 2 characters
+              </span>
+            )}
+          </div>
         </div>
-        <div className='w-full relative'>
-          <input
-            value={room}
-            onChange={(e) => setRoom(e.target.value)}
-            className='border focus:border-indigo-700 p-3 outline-none w-full rounded-lg'
-            type='number'
-            placeholder='Room number'
-          />
-          <AiOutlineInfoCircle
-            onMouseOver={() => setShowPopover(true)}
-            onMouseOut={() => setShowPopover(false)}
-            onClick={() => setShowPopover(!showPopover)}
-            size={25}
-            className='text-gray-400 absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer '
-          />
-          {showPopover && (
-            <div className='absolute right-0 bg-gray-300 text-white p-1 rounded-lg'>
-              <span>The room number must be the same as the other user.</span>
-            </div>
-          )}
+        <div className='w-full flex flex-col'>
+          <div className='w-full relative '>
+            <input
+              value={room}
+              onChange={(e) => setRoom(e.target.value)}
+              className={`${
+                errRoom ? 'border-red-600' : ''
+              } border focus:border-indigo-700 p-3 outline-none w-full rounded-lg`}
+              type='number'
+              min='0'
+              placeholder='Room number'
+            />
+            <AiOutlineInfoCircle
+              onMouseOver={() => setShowPopover(true)}
+              onMouseOut={() => setShowPopover(false)}
+              onClick={() => setShowPopover(!showPopover)}
+              size={25}
+              className='text-gray-400 absolute top-1/2 -translate-y-1/2 right-2 cursor-pointer '
+            />
+            {showPopover && (
+              <div className='absolute right-0 bg-gray-300 text-white p-1 rounded-lg'>
+                <span>The room number must be the same as the other user.</span>
+              </div>
+            )}
+          </div>
+          <div className='errSection h-5'>
+            {errRoom && (
+              <span className='text-xs text-red-600'>*Room is required</span>
+            )}
+          </div>
         </div>
         <div className='w-full p-1'>
           <h3 className='text-sm text-gray-700 pb-5'>Select Avatar</h3>
@@ -79,6 +137,11 @@ function Room({ username, setUsername, room, setRoom, socket }) {
                   </div>
                 );
               })}
+          </div>
+          <div className='errSection h-5'>
+            {errImg && (
+              <span className='text-xs text-red-600'>*avatar is required</span>
+            )}
           </div>
         </div>
         <div
