1ec4bed9a7ac2941ac02550c737d93de74730b80
tugrankenger <turankenger@hotmail.com.tr>
2023-06-06 11:55:04 +0300

    create state for todos and handleToggle function

--- react-typescript-todo/src/components/TodoList.tsx
+++ react-typescript-todo/src/components/TodoList.tsx
@@ -1,13 +1,48 @@
+import { useState } from 'react';
+
+interface item {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
 export const TodoList = () => {
+  const [todos, setTodos] = useState<item[]>([
+    { id: 1, text: 'watch the sopranos', completed: false },
+    { id: 2, text: 'go to cinema', completed: false },
+  ]);
+
+  const handleToggle = (id: number) => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
+  };
   return (
-    <div className="main-container">
+    <div className='main-container'>
       <h1>Todo List</h1>
       <ul>
-        <li>item-1</li>
-        <li>item-2</li>
+        {todos.map((item, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => handleToggle(item.id)}
+              style={{
+                textDecoration: item.completed ? 'line-through' : '',
+                cursor: 'pointer',
+              }}
+            >
+              {item.text}
+            </li>
+          );
+        })}
       </ul>
-      <input type="text" placeholder="Add todo item" />
+      <input type='text' placeholder='Add todo item' />
       <button>Add Todo</button>
     </div>
-  )
+  );
 };
