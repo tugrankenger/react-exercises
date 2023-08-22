495e9c0a046adcf36b75b3c26c35623c5169e1fe
tugrankenger <turankenger@hotmail.com.tr>
2023-08-22 23:19:25 +0300

    fetch data using useReducer and complete project

--- react-reducer/src/App.js
+++ react-reducer/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from 'react';
+import reducer from './reducer';
 
-function App() {
+const initialState = {
+  data: '',
+  loading: false,
+  error: '',
+};
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, loading, error } = state;
+
+  console.log('state: ', state);
+
+  // const [data, setData] = useState('');
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState('');
+
+  const fetchDog = () => {
+    // setLoading(true);
+    // setError('');
+    // setData('');
+
+    dispatch({ type: 'FETCH_START' });
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((res) => {
+        dispatch({ type: 'FETCH_SUCCESS', payload: res.message });
+      })
+      .catch(() => {
+        dispatch({ type: 'FETCH_ERROR', payload: 'Error fetching data' });
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchDog} disabled={loading}>
+        Fetch Dog
+      </button>
+      {data && (
+        <div>
+          <img src={data} alt='random dog image' />
+        </div>
+      )}
+      {error && <p>{error}</p>}
     </div>
   );
 }
-
-export default App;
---
+++ react-reducer/src/reducer.js
@@ -0,0 +1,27 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_START':
+      return {
+        ...state,
+        data: '',
+        loading: true,
+        error: '',
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        data: action.payload,
+        loading: false,
+      };
+    case 'FETCH_ERROR':
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
